48b5c4811574f0ce153c2fef82521d6bcdcc3a4c
Kclient <go4521304@gmail.com>
2021-12-05 20:44:37 +0900

    Actor 생성

---
+++ Prototype/Intermediate/Build/Win64/PrototypeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.30.30705
+#include "C:/Users/Kclient/Desktop/Git/For_graduation/Prototype/Intermediate/Build/Win64/PrototypeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Prototype/Source/Prototype/MyActor.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MESH"));
+
+	RootComponent = Mesh;
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> SM(TEXT("StaticMesh'/Game/MY/Actor/girl_lowpoly1.girl_lowpoly1'"));
+
+	if (SM.Succeeded())
+	{
+		Mesh->SetStaticMesh(SM.Object);
+	}
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
