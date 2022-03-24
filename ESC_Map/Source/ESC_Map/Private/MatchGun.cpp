efe4c83b35e3d1a0e32f23fdabdeefc0ef6a05d6
Kclient <go4521304@gmail.com>
2022-03-25 02:05:53 +0900

    여자 캐릭터 완성
    - VR 폰
    - 무기
    - 발사체
    - 무기 스코프

--- ESC_Map/Source/ESC_Map/Private/MatchGun.cpp
+++ ESC_Map/Source/ESC_Map/Private/MatchGun.cpp
@@ -9,7 +9,7 @@
 AMatchGun::AMatchGun()
 {
 	InitialLifeSpan = 5.0f;
-	
+
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
@@ -17,11 +17,12 @@ AMatchGun::AMatchGun()
 	CollisionComponent->BodyInstance.SetCollisionProfileName(TEXT("Projecile"));
 	CollisionComponent->OnComponentHit.AddDynamic(this, &AMatchGun::OnHit);
 	CollisionComponent->SetSphereRadius(15.0f);
-	
+
 	RootComponent = CollisionComponent;
 
 	// ProjectileMovementComponent 를 사용하여 이 발사체의 운동을 관장합니다.
-	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(
+		TEXT("ProjectileMovementComponent"));
 	ProjectileMovementComponent->SetUpdatedComponent(CollisionComponent);
 	ProjectileMovementComponent->InitialSpeed = 3000.0f;
 	ProjectileMovementComponent->MaxSpeed = 3000.0f;
@@ -40,11 +41,6 @@ void AMatchGun::BeginPlay()
 void AMatchGun::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	if (isHit)
-	{
-		cmp->AddImpulseAtLocation(FVector(FVector2D(vel), 0.f), this->GetActorLocation());
-	}
 }
 
 void AMatchGun::FireInDirection(const FVector& ShootDirection)
@@ -53,20 +49,19 @@ void AMatchGun::FireInDirection(const FVector& ShootDirection)
 }
 
 void AMatchGun::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent,
-	FVector NormalImpulse, const FHitResult& Hit)
+                      FVector NormalImpulse, const FHitResult& Hit)
 {
 	if (OtherActor != this)
 	{
-			UE_LOG(LogTemp, Warning, TEXT("TEST"));
-			AttachToActor(OtherActor, FAttachmentTransformRules::KeepWorldTransform);
+		UE_LOG(LogTemp, Warning, TEXT("TEST"));
+		AttachToActor(OtherActor, FAttachmentTransformRules::KeepWorldTransform);
 	}
 	if (OtherActor != this && OtherComponent->IsSimulatingPhysics())
 	{
-		//OtherComponent->AddImpulseAtLocation(ProjectileMovementComponent->Velocity, Hit.ImpactPoint);
+		OtherComponent->AddImpulseAtLocation(ProjectileMovementComponent->Velocity * power, Hit.ImpactPoint);
 		cmp = OtherComponent;
 		vel = ProjectileMovementComponent->Velocity;
 		isHit = true;
 	}
 	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Blue, TEXT("TEST"));
 }
-
